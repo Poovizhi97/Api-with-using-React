c08a23780a217067dd75142465c30b8a83e096ba
poovizhi97 <poovizhi.aarya@gmail.com>
2020-07-04 14:39:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component ,Fragment} from 'react';
+import axois from "axios"; 
+import "./COMPONENT/Spinner";
+import Spinner from './COMPONENT/Spinner';
+class App extends Component{
+    constructor (props){
+        super(props);
+        this.state={
+            items:[],
+            loading:false
+        };
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    async componentDidMount(){
+        //window.fetch is only for frontend
+        //axios is working with frontend and backend node js https request get post dellete,header
+        //let response =await window.fetch("https://api.github.com/users");
+        //console.log(response);
+      // let json=await response.json();
+       //console.log(json)
+
+
+       let axiosResponse=await axois.get("https://api.github.com/users");
+      // console.log(axiosResponse);
+      this.setState({items:axiosResponse.data,loading:true});
+    }
+    
+    render(){
+        //useing loop method 
+        let {loading,items}=this.state;
+        //loop
+        let GitUser=items.map((user)=>{
+            return(
+                <Fragment key={user.id}>
+                    <div className="card">
+                        <img className="card-img-top" 
+                        src={user.avatar_url} 
+                        alt={user.name}/>
+                        <div className="card-body">
+                            <h4 className="display-5 font weight-bold">{user.login}</h4>
+                        </div>
+                    </div>
+                </Fragment>
+            );
+        });
+
+    
+        return(
+            <Fragment>
+                {loading?
+                (
+                 <section className="container my-4 d-flex flex-wrap justify-content-between">
+                     {GitUser}
+                     </section>   
+                ):(
+                    <Spinner/>
+                )}
+            </Fragment>
+        );
+
+    }
 }
-
 export default App;
---
+++ src/COMPONENT/Spinner.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./spinner.styles.css";
+
+
+let Spinner=()=> {
+    return (
+        <div className="container">
+<div className="flex">
+<div className="loader">
+</div>
+</div>
+<div className="load-text">
+Loading...
+</div>
+</div>
+    )
+}
+
+export default Spinner
